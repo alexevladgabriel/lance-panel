a7fb544a2a27e3ef60c1cfb367a9f78942f3cf5a
Lance Pioch <git@lance.sh>
2024-03-06 13:12:03 -0500

    Initial

---
+++ app/Http/Controllers/Admin/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\View\View;
+use Illuminate\View\Factory as ViewFactory;
+use App\Http\Controllers\Controller;
+use App\Services\Helpers\SoftwareVersionService;
+
+class BaseController extends Controller
+{
+    /**
+     * BaseController constructor.
+     */
+    public function __construct(private SoftwareVersionService $version, private ViewFactory $view)
+    {
+    }
+
+    /**
+     * Return the admin index view.
+     */
+    public function index(): View
+    {
+        return $this->view->make('admin.index', ['version' => $this->version]);
+    }
+}
---
+++ app/Http/Controllers/Admin/Nodes/NodeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Nodes;
+
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Models\Node;
+use Spatie\QueryBuilder\QueryBuilder;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+
+class NodeController extends Controller
+{
+    /**
+     * NodeController constructor.
+     */
+    public function __construct(private ViewFactory $view)
+    {
+    }
+
+    /**
+     * Returns a listing of nodes on the system.
+     */
+    public function index(Request $request): View
+    {
+        $nodes = QueryBuilder::for(
+            Node::query()->with('location')->withCount('servers')
+        )
+            ->allowedFilters(['uuid', 'name'])
+            ->allowedSorts(['id'])
+            ->paginate(25);
+
+        return $this->view->make('admin.nodes.index', ['nodes' => $nodes]);
+    }
+}
